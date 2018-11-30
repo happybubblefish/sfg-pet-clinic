6a5af15469101467dafd13193a4070dc4eb2a4af
Lin Cong <lin.cong@finra.org>
2018-11-30 13:35:46 -0500

    Implement services. Closes #5.

---
+++ pet-clinic-data/src/main/java/finra/springframework/sfgpetclinic/sevices/PetService.java
@@ -0,0 +1,14 @@
+package finra.springframework.sfgpetclinic.sevices;
+
+import finra.springframework.sfgpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
---
+++ pet-clinic-data/src/main/java/finra/springframework/sfgpetclinic/sevices/VetService.java
@@ -0,0 +1,14 @@
+package finra.springframework.sfgpetclinic.sevices;
+
+import finra.springframework.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
