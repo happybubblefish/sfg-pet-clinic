cd6b293dec2bc49722d2eaa04fe3ee83eca15847
happybubblefish <congls2007@yahoo.com>
2018-12-01 02:41:49 -0500

    Update Map Impl to manage setting of ID Property. Closes #23.

--- pet-clinic-data/src/main/java/finra/springframework/sfgpetclinic/sevices/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/finra/springframework/sfgpetclinic/sevices/map/AbstractMapService.java
@@ -1,13 +1,13 @@
 package finra.springframework.sfgpetclinic.sevices.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import finra.springframework.sfgpetclinic.model.BaseEntity;
 
-public abstract class AbstractMapService<T, ID> {
+import java.util.*;
 
-    protected Map<ID, T> map = new HashMap<>();
+//The purpose of extends here is to let object be able to call getId(), and map.ketset() be able to + 1
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
+
+    protected Map<Long, T> map = new HashMap<>();
 
     Set<T> findAll() {
         return new HashSet<>(map.values());
@@ -17,8 +17,17 @@ public abstract class AbstractMapService<T, ID> {
         return map.get(id);
     }
 
-    T save(ID id, T object) {
-        map.put(id, object);
+    T save(T object) {
+
+        if(object != null) {
+            if(object.getId() == null) {
+                object.setId(getNextId());
+            }
+
+            map.put(object.getId(), object);
+        } else {
+            throw new RuntimeException("Object cannot be null.");
+        }
 
         return object;
     }
@@ -29,5 +38,17 @@ public abstract class AbstractMapService<T, ID> {
 
     void delete(T object) {
         map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+    }
+
+    private Long getNextId() {
+        Long nextId = null;
+
+        try{
+            nextId = Collections.max(map.keySet()) + 1;
+        } catch (NoSuchElementException e){
+            nextId = 1L;
+        }
+
+        return nextId;
     }
 }
