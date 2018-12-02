a5cc63620f519020ab12de48b1425073871385ec
happybubblefish <congls2007@yahoo.com>
2018-12-02 00:45:55 -0500

    Create PetType Map Service Impl. Closes #29.

---
+++ pet-clinic-data/src/main/java/finra/springframework/sfgpetclinic/sevices/PetTypeService.java
@@ -0,0 +1,6 @@
+package finra.springframework.sfgpetclinic.sevices;
+
+import finra.springframework.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
